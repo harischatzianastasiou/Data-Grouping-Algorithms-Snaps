dc89dc0e6e08b4718ba04ddbcc01c657a8a5df2f
Haris Chatzianastasiou <75744098+harischatzianastasiou@users.noreply.github.com>
2024-05-04 15:34:26 +0300

    Create  setup.py

---
+++ Snaps/flasktemp/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+        name='flasktemp',
+        version='1.0.0',
+        description='
+                Runs an HTTP Flood Python script that could stop
+                the webpage used in flasktemp',
+        author='tchatzian',
+        author_email='harischatzi1988@gmail.com',
+        url='https://github.com/tchatzian/flasktemp',
+        packages=['flasktemp'],
+        include_package_data=True,
+        zip_safe=False,
+        scripts=['flasktemp/pyflooder.py']
+)
